3eca873053ec8c130f5544a0cc3c74673f973ae6
egoMasa <jeremyfournier203@gmail.com>
2026-02-08 22:18:25 +0100

    v1

--- ansible/artifacts/join-workers.sh
+++ ansible/artifacts/join-workers.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kubeadm join 192.168.122.150:6443 --token cgzl22.jwfttn82a92jssgd --discovery-token-ca-cert-hash sha256:9b5dc4f88152fc6f87f3b20915fdfeef77297a69cfc5ff9b22a38e6ee75c074f 
+kubeadm join 192.168.122.150:6443 --token fk4wtt.j2upqvjdggi158gp --discovery-token-ca-cert-hash sha256:7c4029f12b9657296f62a5220e384039e32d9581deffd39fd015c62f9c81dce3 
